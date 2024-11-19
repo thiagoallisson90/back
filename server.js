d8505fdda42cda641c59b6c670a8bd161128a9e2
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2024-11-19 01:14:35 -0300

    Commit

---
+++ server.js
@@ -0,0 +1,31 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+//import path from "path";
+//import { fileURLToPath } from "url";
+
+const app = express();
+
+app.use(json());
+app.use(cors());
+dotenv.config();
+
+const port = process.env.PORT || 3100;
+/*const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);*/
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    ok: true,
+    message: "Acessing API",
+  });
+});
+
+/*app.get("/index.html", (req, res) => {
+  const filePath = path.join(__dirname, "public", "index.html");
+  res.status(200).sendFile(filePath);
+});*/
+
+app.listen(port, () => {
+  console.log(`Server running in port ${port}`);
+});
